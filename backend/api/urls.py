aa7c3e96341bafa4a57823fe8f4e592861ba67a0
moonburnt <moonburnt@disroot.org>
2023-08-16 17:50:34 +0300

    reorganize bookmarks in collection

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,17 +1,25 @@
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
+from rest_framework_nested import routers
 from . import views
 import logging
 
 log = logging.getLogger(__name__)
 
 
-router = DefaultRouter()
+router = routers.DefaultRouter()
 router.register("auth/register", views.RegisterUserView)
 router.register("bookmarks", views.BookmarkViewSet)
 router.register("collections", views.BookmarkCollectionViewSet)
 
+bookmarks_in_collection_counter = routers.NestedSimpleRouter(
+    parent_router=router,
+    parent_prefix="collections",
+    lookup="collection",
+)
+bookmarks_in_collection_counter.register("bookmark", views.BookmarkInCollectionViewSet)
+
 urlpatterns = [
     path("", include(router.urls)),
+    path("", include(bookmarks_in_collection_counter.urls)),
     path("auth/", include("rest_framework.urls")),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.contrib.auth import get_user_model
 from django.http import HttpResponseRedirect
 from rest_framework import status, viewsets, mixins
@@ -20,6 +21,16 @@ class IsUnauthenticated(BasePermission):
         return not (request.user and request.user.is_authenticated)
 
 
+class CreateRetrieveListDestroyViewSet(
+    mixins.CreateModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.ListModelMixin,
+    mixins.DestroyModelMixin,
+    viewsets.GenericViewSet,
+):
+    pass
+
+
 class RegisterUserView(CreateOnlyModelViewSet):
     serializer_class = serializers.RegisterUserSerializer
     permission_classes = [IsUnauthenticated]
@@ -39,8 +50,6 @@ class BookmarkViewSet(viewsets.ModelViewSet):
     def get_serializer_class(self):
         if self.action == "create":
             return serializers.AddBookmarkSerializer
-        elif self.action in ("update", "partial_update",):
-            return serializers.AddToCollectionBookmarkSerializer
         else:
             return super().get_serializer_class()
 
@@ -49,3 +58,41 @@ class BookmarkCollectionViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.BookmarkCollectionSerializer
     permission_classes = [IsAuthenticated]
     queryset = models.BookmarkCollectionModel.objects.all()
+
+
+class BookmarkInCollectionViewSet(CreateRetrieveListDestroyViewSet):
+    serializer_class = serializers.BookmarkSerializer
+    permission_classes = [IsAuthenticated]
+    queryset = models.BookmarkModel.objects.all()
+
+    def get_queryset(self):
+        if self.action == "create":
+            return models.BookmarkModel.objects.filter(~Q(collections=self.kwargs['collection_pk']))
+        else:
+            return models.BookmarkModel.objects.filter(collections=self.kwargs['collection_pk'])
+
+    def get_serializer_class(self):
+        if self.action == "create":
+            return serializers.AddToCollectionBookmarkSerializer
+        else:
+            return super().get_serializer_class()
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer.validated_data["bookmark_id"])
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, headers=headers)
+
+    def perform_create(self, obj):
+        obj.collections.add(self.kwargs['collection_pk'])
+
+    def perform_destroy(self, instance):
+        instance.collections.remove(self.kwargs['collection_pk'])
+
+
